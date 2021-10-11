e7444886a49a426af62c41b6ddc5b6516c3fbd31
bhill7770 <91581195+bhill7770@users.noreply.github.com>
2021-10-10 22:26:17 -0400

    Create UserList.java

---
+++ UserList.java
@@ -0,0 +1,26 @@
+package com.example.truelifemobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+public class UserList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    public UserList(){
+
+    }
+
+    @Override
+    protected void onCreate(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflator.inflate(R.layout.fragment_users, container, );
+
+@Override
+        public View onCreateView(LayoutInflater inflator, ViewGroup container)
+    }
+}
